ec2041931bb79ccc3130fb4f0c125225cc35244c
Henry <henrysungyun@gmail.com>
2023-02-04 12:01:04 -0800

    slowly but surely

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("to bathe my dog")
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork("take a day off")
+
+function wrapAdjective(value = "*") {
+    return function(adj = "special") {
+        return `You are ${value}${adj}${value}!`
+    }
+} 
+wrapAdjective("%")("a dedicated programmer");
